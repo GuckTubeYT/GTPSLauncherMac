48382eada99adab57e022d75d04c37dd64955fa4
GuckTube YT <56192597+GuckTubeYT@users.noreply.github.com>
2020-05-13 15:22:23 +0700

    Add files via upload

---
+++ gtis.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+ ######   ######## ########   ######  ##          ###    ##     ## ##    ##  ######  ##     ## ######## ########  
+##    ##     ##    ##     ## ##    ## ##         ## ##   ##     ## ###   ## ##    ## ##     ## ##       ##     ## 
+##           ##    ##     ## ##       ##        ##   ##  ##     ## ####  ## ##       ##     ## ##       ##     ## 
+##   ####    ##    ########   ######  ##       ##     ## ##     ## ## ## ## ##       ######### ######   ########  
+##    ##     ##    ##              ## ##       ######### ##     ## ##  #### ##       ##     ## ##       ##   ##   
+##    ##     ##    ##        ##    ## ##       ##     ## ##     ## ##   ### ##    ## ##     ## ##       ##    ##  
+ ######      ##    ##         ######  ######## ##     ##  #######  ##    ##  ######  ##     ## ######## ##     ## 
+# GTPSLauncher For Mac (C) GuckTube YT
+# Dont forget to Credit me
+
+if [ "$EUID" -ne 0 ]
+  then echo "ERROR: Please Run as Sudo/Root, Type 'sudo ./name.sh'"
+  exit
+fi
+clear
+echo ""
+echo "##    ##    ###    ##     ## ######## "
+echo "###   ##   ## ##   ###   ### ##       "
+echo "####  ##  ##   ##  #### #### ##       "
+echo "## ## ## ##     ## ## ### ## ######   "
+echo "##  #### ######### ##     ## ##       "
+echo "##   ### ##     ## ##     ## ##       "
+echo "##    ## ##     ## ##     ## ######## "
+echo ""
+PS3="Please Select Number (1-3): "
+select shuttle in "Turn On" "Turn Off" "Exit"
+do
+    case $shuttle in
+                "Turn On")
+                        echo ""
+                        echo $(hostname -I | cut -d\  -f1) $(hostname) | sudo tee -a /etc/hosts
+                        sudo echo "# GTPS Launcher for Mac/Linux by GuckTube YT" >> /etc/hosts
+                        sudo echo "# GTPS Is Turn On" >> /etc/hosts
+                        sudo echo "IPHOST growtopia1.com" >> /etc/hosts
+                        sudo echo "IPHOST growtopia2.com" >> /etc/hosts
+						                        echo "GTPS Is Turn On"                                                                                                  ;;
+                "Turn Off")
+                        echo ""
+                        echo $(hostname -I | cut -d\  -f1) $(hostname) | sudo tee -a /etc/hosts
+                        sudo echo "# GTPS Launcher for Mac/Linux by GuckTube YT" >> /etc/hosts
+                        sudo echo "# GTPS Is Turn Off" >> /etc/hosts
+                        echo "GTPS Is Turn Off"
+                        ;;
+                "Exit")
+                        exit
+                        ;;
+                *)
+                        echo "Error: Wrong Number (select 1 - 3)"
+                        echo ""
+                        echo "##    ##    ###    ##     ## ######## "
+						echo "###   ##   ## ##   ###   ### ##       "
+						echo "####  ##  ##   ##  #### #### ##       "
+						echo "## ## ## ##     ## ## ### ## ######   "
+						echo "##  #### ######### ##     ## ##       "
+						echo "##   ### ##     ## ##     ## ##       "
+						echo "##    ## ##     ## ##     ## ######## "
+                        echo ""
+                        ;;
+        esac
+done
